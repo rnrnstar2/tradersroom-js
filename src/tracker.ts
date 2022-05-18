de3b85a17667e87cff1c5f8c5edf49f9085f7fde
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2022-05-18 13:50:36 +0900

    add

--- src/tracker.ts
+++ src/tracker.ts
@@ -16,31 +16,31 @@ export default class Tracker {
     console.log(this.publicKey);
   }
 
-  // API送信
-  async postApi(sendData: any) {
-    console.log(">>> postApi <<<");
-    return new Promise((resolve, reject) => {
-      fetch("https://ett62q7csgeatvki2ajoegwyoe0cnpoa.lambda-url.ap-northeast-1.on.aws/ ", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: JSON.stringify({
-          ...sendData,
-          publicKey: this.publicKey,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Success:", data);
-          resolve(data);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          reject("fail");
-        });
-    });
-  }
+  // // API送信
+  // async postApi(sendData: any) {
+  //   console.log(">>> postApi <<<");
+  //   return new Promise((resolve, reject) => {
+  //     fetch("https://ett62q7csgeatvki2ajoegwyoe0cnpoa.lambda-url.ap-northeast-1.on.aws/ ", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/x-www-form-urlencoded",
+  //       },
+  //       body: JSON.stringify({
+  //         ...sendData,
+  //         publicKey: this.publicKey,
+  //       }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log("Success:", data);
+  //         resolve(data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error:", error);
+  //         reject("fail");
+  //       });
+  //   });
+  // }
 
   // トラッキングIDを取得
   getTrackingId() {
@@ -68,23 +68,23 @@ export default class Tracker {
       if (!trackingId) return;
       console.log("trackingId: ", trackingId);
 
-      // フォームデータを解析
-      const target = event.target;
-      if (!target) return;
-      const arr = Array.from(target as any) as HTMLInputElement[];
-      let sendData: any = {};
-      arr.map((e) => {
-        const value = e.value;
-        const attribute = e.attributes as any;
-        const key = attribute["data-tr-form"]?.value;
-        if (key) sendData[key] = value;
-      });
-      console.log("sendData: ", sendData);
+      // // フォームデータを解析
+      // const target = event.target;
+      // if (!target) return;
+      // const arr = Array.from(target as any) as HTMLInputElement[];
+      // let sendData: any = {};
+      // arr.map((e) => {
+      //   const value = e.value;
+      //   const attribute = e.attributes as any;
+      //   const key = attribute["data-tr-form"]?.value;
+      //   if (key) sendData[key] = value;
+      // });
+      // console.log("sendData: ", sendData);
 
-      // チェック
-      if (!sendData.email) return;
+      // // チェック
+      // if (!sendData.email) return;
 
-      // API送信
+      // // API送信
       // await this.postApi(sendData);
     });
   }
