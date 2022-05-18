a9b154a840e14ca21d8c2eb73916fa24eb61adbd
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2022-05-18 13:46:09 +0900

    add

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,7 +1,4 @@
-// interface SendData {
-//   email: string;
-//   name?: string;
-// }
+// interface は使えない？
 
 export default class Tracker {
   publicKey: string;
@@ -20,24 +17,29 @@ export default class Tracker {
   }
 
   // API送信
-  postApi(sendData: any) {
-    fetch("https://ett62q7csgeatvki2ajoegwyoe0cnpoa.lambda-url.ap-northeast-1.on.aws/ ", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: JSON.stringify({
-        ...sendData,
-        publicKey: this.publicKey,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
+  async postApi(sendData: any) {
+    console.log(">>> postApi <<<");
+    return new Promise((resolve, reject) => {
+      fetch("https://ett62q7csgeatvki2ajoegwyoe0cnpoa.lambda-url.ap-northeast-1.on.aws/ ", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify({
+          ...sendData,
+          publicKey: this.publicKey,
+        }),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          resolve(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          reject("fail");
+        });
+    });
   }
 
   // トラッキングIDを取得
@@ -55,8 +57,9 @@ export default class Tracker {
 
   // 証券会社に登録
   form() {
+    console.log("formのリスナーを起動しました。");
     const formElement = <HTMLFormElement>document.getElementById("tr-form");
-    formElement.addEventListener("submit", (event) => {
+    formElement.addEventListener("submit", async (event) => {
       console.log("フォームがsubmitされました");
       event.preventDefault();
 
@@ -82,7 +85,7 @@ export default class Tracker {
       if (!sendData.email) return;
 
       // API送信
-      this.postApi(sendData);
+      // await this.postApi(sendData);
     });
   }
 }
