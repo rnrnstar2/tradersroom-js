5c4032b437492905a1f2e980e93168171e04b02e
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2022-05-25 18:44:18 +0900

    add

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,86 @@ import setCookie from "./setCookie";
 import getQuery from "./getQuery";
 
 // クラス
-import Tracker from "./tracker";
 import FormListener from "./formListener";
+import { fetchJSON } from "./api-helpers";
 
-const cookieInit = () => {
-  console.log(">>> cookieInit <<<");
-  // トラッキングIdがクエリに存在する場合はcookieにセット
-  const projectId = getQuery("projectId");
-  const memberId = getQuery("memberId");
-  const trackingId = getQuery("trackingId");
-  if (projectId) setCookie("projectId", projectId, 30);
-  if (memberId) setCookie("memberId", memberId, 30);
-  if (trackingId) setCookie("trackingId", trackingId, 30);
-};
+// 辞書オブジェクトを作成する
+interface StringDict {
+  [name: string]: string;
+}
 
-const trackerInit = (programId: string) => {
-  console.log(">>> trackerInit <<<");
-  return new Tracker(programId);
-};
+export default class TradersRoomPartner {
+  programId: string;
+  constructor(programId: string) {
+    this.programId = programId;
+  }
 
-const formInit = (programId: string) => {
-  console.log(">>> formInit <<<");
-  cookieInit();
-  const formListener = new FormListener(programId);
-  formListener.form();
-};
+  cookieInit() {
+    console.log(">>> cookieInit <<<");
+    // トラッキングIdがクエリに存在する場合はcookieにセット
+    const projectId = getQuery("projectId");
+    const memberId = getQuery("memberId");
+    const trackingId = getQuery("trackingId");
+    if (projectId) setCookie("projectId", projectId, 30);
+    if (memberId) setCookie("memberId", memberId, 30);
+    if (trackingId) setCookie("trackingId", trackingId, 30);
+  }
 
-export default { trackerInit, formInit };
+  formInit(programId: string) {
+    console.log(">>> formInit <<<");
+    this.cookieInit();
+    const formListener = new FormListener(programId);
+    formListener.form();
+  }
+
+  createTrader(memberId: string, projectId?: string, trackingId?: string) {
+    console.log(memberId, projectId, trackingId);
+    let input: StringDict = {
+      memberId: memberId,
+      programId: this.programId,
+    };
+    if (projectId) input["projectId"] = projectId;
+    if (trackingId) input["trackingId"] = trackingId;
+    fetchJSON("PUT", "trader", input);
+  }
+
+  updateTrader(email: string, accountNumber: string) {
+    console.log(email, accountNumber);
+    let input: StringDict = {
+      email: email,
+      accountNumber: accountNumber,
+      programId: this.programId,
+    };
+    fetchJSON("POST", "trader", input);
+  }
+
+  updateProgramMembers(programMembersId: string, pips: string) {
+    console.log(programMembersId, pips);
+    let input: StringDict = {
+      programMembersId: programMembersId,
+      pips: pips,
+      programId: this.programId,
+    };
+    fetchJSON("POST", "programMembers", input);
+  }
+
+  createTraderReport(accountNumber: string, amount: string, lot?: string) {
+    console.log(accountNumber, amount, lot);
+    let input: StringDict = {
+      accountNumber: accountNumber,
+      programId: this.programId,
+      amount: amount,
+    };
+    if (lot) input["lot"] = lot;
+    fetchJSON("PUT", "traderReport", input);
+  }
+
+  user(name: string) {
+    console.log("name: ", name);
+  }
+
+  track() {
+    console.log("track");
+    console.log(this.programId);
+  }
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,11 @@
-export class Tracker {
+export class TradersRoomPartner {
   constructor(programId: string);
+  public cookieInit(): void;
+  public formInit(programId: string): void;
   public createTrader(memberId: string, projectId?: string, trackingId?: string): void;
   public updateTrader(email: string, accountNumber: string): void;
   public updateProgramMembers(programMembersId: string, pips: string): void;
   public createTraderReport(accountNumber: string, amount: string, lot?: string): void;
   public user(name: string): void;
+  public track(): void;
 }
