68103ede04163687a2fb5d6c89efd8f5460bec4c
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2022-06-30 14:40:53 +0900

    add

--- src/api-helpers.ts
+++ src/api-helpers.ts
@@ -2,24 +2,6 @@ import type { StringDict } from '../types/index';
 const trackerLink =
   'https://avjby6ynal.execute-api.ap-northeast-1.amazonaws.com/dev';
 
-// const handleErrors = (res: Response) => {
-//   if (res.ok) return res;
-//   switch (res.status) {
-//     case 400:
-//       throw Error('INVALID_TOKEN');
-//     case 401:
-//       throw Error('UNAUTHORIZED');
-//     case 500:
-//       throw Error('INTERNAL_SERVER_ERROR');
-//     case 502:
-//       throw Error('BAD_GATEWAY');
-//     case 404:
-//       throw Error('NOT_FOUND');
-//     default:
-//       throw Error('UNHANDLED_ERROR');
-//   }
-// };
-
 export async function fetchJSON(
   mode: 'POST' | 'PUT',
   query: string,
@@ -30,7 +12,7 @@ export async function fetchJSON(
     const response = await fetch(`${trackerLink}/${query}`, {
       method: mode,
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json', // これにしないとlambdaでbodyがnullになる!!
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(body),
@@ -47,27 +29,4 @@ export async function fetchJSON(
   } catch (error) {
     throw error;
   }
-
-  // fetch(`${trackerLink}/${query}`, {
-  //   method: mode,
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     Authorization: `Bearer ${token}`,
-  //   },
-  //   body: JSON.stringify(body),
-  // })
-  //   // 1. ネットワーク周りなどのリクエスト以前の段階でのエラーを処理する
-  //   .catch((e) => {
-  //     throw Error(e);
-  //   })
-  //   // 2. サーバサイドで発行されたエラーステータスを処理する
-  //   .then(handleErrors)
-  //   // 3. 以上2つをパスした正常なレスポンスからJSONオブジェクトをパースする
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log('Success: ', data);
-  //   })
-  //   .catch((error) => {
-  //     console.log('Error: ', error);
-  //   });
 }
--- src/partners.ts
+++ src/partners.ts
@@ -1,9 +1,9 @@
 import type { Partner as IPartner, StringDict } from '../types/index';
-import setCookie from "./setCookie";
-import getQuery from "./getQuery";
+import setCookie from './setCookie';
+import getQuery from './getQuery';
 
 // クラス
-import FormListener from "../src/formListener";
+import FormListener from '../src/formListener';
 import { fetchJSON } from './api-helpers';
 
 export class Partner implements IPartner {
@@ -12,18 +12,18 @@ export class Partner implements IPartner {
     this.token = token;
   }
   public cookieInit(): void {
-    console.log(">>> cookieInit <<<");
+    console.log('>>> cookieInit <<<');
     // トラッキングIdがクエリに存在する場合はcookieにセット
-    const projectId = getQuery("projectId");
-    const memberId = getQuery("memberId");
-    const trackingId = getQuery("trackingId");
-    if (projectId) setCookie("projectId", projectId, 30);
-    if (memberId) setCookie("memberId", memberId, 30);
-    if (trackingId) setCookie("trackingId", trackingId, 30);
+    const projectId = getQuery('projectId');
+    const memberId = getQuery('memberId');
+    const trackingId = getQuery('trackingId');
+    if (projectId) setCookie('projectId', projectId, 30);
+    if (memberId) setCookie('memberId', memberId, 30);
+    if (trackingId) setCookie('trackingId', trackingId, 30);
   }
 
   public formInit(programId: string): void {
-    console.log(">>> formInit <<<");
+    console.log('>>> formInit <<<');
     this.cookieInit();
     const formListener = new FormListener(programId);
     formListener.form();
@@ -35,18 +35,18 @@ export class Partner implements IPartner {
   }
 
   public updateTrader(input: StringDict): void {
-    console.log("updateTrader", input);
-    fetchJSON("POST", "trader", input, this.token);
+    console.log('updateTrader', input);
+    fetchJSON('POST', 'trader', input, this.token);
   }
 
   public updateProgramMembers(input: StringDict): void {
-    console.log("updateProgramMembers", input);
-    fetchJSON("POST", "programMembers", input, this.token);
+    console.log('updateProgramMembers', input);
+    fetchJSON('POST', 'programMembers', input, this.token);
   }
 
   public createTraderReport(input: StringDict): void {
-    console.log("createTraderReport", input);
-    fetchJSON("PUT", "traderReport", input, this.token);
+    console.log('createTraderReport', input);
+    fetchJSON('PUT', 'traderReport', input, this.token);
   }
 
   public user(name: string): void {
