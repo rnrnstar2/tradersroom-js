b399837ab968d1f2b81bc30086584fa86964866b
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2022-07-01 16:24:34 +0900

    add

--- tsdx.config.js
+++ tsdx.config.js
@@ -4,9 +4,11 @@ module.exports = {
     const extension = '.' + config.output.file.split('.').slice(1).join('.');
     let filename = config.input.split('src/')[1]; // remove src/
     filename = filename.split('.')[0]; // remove extension, if any
-
+    if (config.output.format === 'umd') {
+      delete config.external;
+    }
     config.output.file = outputDir + filename + extension;
-    console.log(config.output.file);
+    console.log(filename, config.output.file);
     // replace / with __ for UMD names
     config.output.name = filename.replace('/', '__');
     return config;
